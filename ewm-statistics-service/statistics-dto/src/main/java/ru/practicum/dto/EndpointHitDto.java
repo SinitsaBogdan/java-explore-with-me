1b11f075dfdbf58d62592a04e9f95cacb7895703
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-11-16 01:59:06 +0300

    add : module statistics dto

---
+++ ewm-statistics-service/statistics-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -0,0 +1,37 @@
+package ru.practicum.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Validated
+@AllArgsConstructor
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EndpointHitDto {
+
+    @NotBlank(message = "Параметр app не должен быть пустым")
+    private String app;
+
+    @NotBlank(message = "Параметр uri не должен быть пустым")
+    private String uri;
+
+    @NotBlank(message = "Параметр ip не должен быть пустым")
+    private String ip;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("timestamp")
+    private final LocalDateTime timestamp;
+}
---
+++ ewm-statistics-service/statistics-dto/src/test/java/ru/practicum/dto/EndpointHitDtoTest.java
@@ -0,0 +1,43 @@
+package ru.practicum.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+@ContextConfiguration(classes = {EndpointHitDtoTest.class})
+public class EndpointHitDtoTest {
+
+    @Autowired
+    private JacksonTester<EndpointHitDto> endpointHitDtoJacksonTester;
+
+    @Test
+    void endpointHitDtoTest() throws IOException {
+        LocalDateTime timestamp = LocalDateTime.now();
+
+        EndpointHitDto endpointHitDto = EndpointHitDto.builder()
+                .app("app")
+                .uri("uri")
+                .ip("127.0.0.1")
+                .timestamp(timestamp)
+                .build();
+
+        JsonContent<EndpointHitDto> jsonContent = endpointHitDtoJacksonTester.write(endpointHitDto);
+
+        String formattedTimestamp = timestamp.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        assertThat(jsonContent).extractingJsonPathStringValue("$.app").isEqualTo("app");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.uri").isEqualTo("uri");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.ip").isEqualTo("127.0.0.1");
+        assertThat(jsonContent).extractingJsonPathStringValue("$.timestamp").isEqualTo(formattedTimestamp);
+    }
+}
