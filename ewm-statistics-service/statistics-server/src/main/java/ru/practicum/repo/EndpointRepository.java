7b548b52e9f211883195f9857b4ae2e19a4dc0d4
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-11-16 02:45:49 +0300

    Update EndpointRepository.java

--- ewm-statistics-service/statistics-server/src/main/java/ru/practicum/repo/EndpointRepository.java
+++ ewm-statistics-service/statistics-server/src/main/java/ru/practicum/repo/EndpointRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.repo;
 
 import ru.practicum.dto.ViewEndpointDto;
-import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.model.EndpointHit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +11,7 @@ import java.util.List;
 
 public interface EndpointRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
+    @Query("SELECT new ru.practicum.dto.ViewEndpointDto(h.app, h.uri, COUNT(h.ip)) " +
             "FROM EndpointHit AS h " +
             "WHERE h.timestamp BETWEEN (:start) AND (:end) " +
             "AND (COALESCE(:uris, null) IS NULL OR h.uri IN (:uris)) " +
@@ -20,7 +19,7 @@ public interface EndpointRepository extends JpaRepository<EndpointHit, Long> {
             "ORDER BY 3 DESC ")
     List<ViewEndpointDto> findEndpoint(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
-    @Query("SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(distinct h.ip)) " +
+    @Query("SELECT new ru.practicum.dto.ViewEndpointDto(h.app, h.uri, COUNT(distinct h.ip)) " +
             "FROM EndpointHit AS h " +
             "WHERE h.timestamp BETWEEN (:start) AND (:end) " +
             "AND (COALESCE(:uris, null) IS NULL OR h.uri IN (:uris)) " +
