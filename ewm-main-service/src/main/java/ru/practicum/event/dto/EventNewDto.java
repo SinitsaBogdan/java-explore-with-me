18d434d875500b84a1d8a97eb456204ed945164a
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-11-27 19:37:02 +0300

    refactor : EventNewDto.java
    rev : Можно валидировать при помощи @min и @max для того, чтобы задать диапазон допустимых значений

--- ewm-main-service/src/main/java/ru/practicum/event/dto/EventNewDto.java
+++ ewm-main-service/src/main/java/ru/practicum/event/dto/EventNewDto.java
@@ -10,6 +10,7 @@ import org.springframework.validation.annotation.Validated;
 import ru.practicum.location.dto.LocationDto;
 import ru.practicum.util.Constants;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -22,7 +23,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class EventNewDto {
 
-    @NotNull
+    @Min(1)
     private Long category;
 
     @NotNull
