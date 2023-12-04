b6a1794bef38ef79f6662cf38fd9dbab09deb171
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-12-04 22:25:33 +0300

    Create Comment.java

---
+++ ewm-main-service/src/main/java/ru/practicum/comment/model/Comment.java
@@ -0,0 +1,37 @@
+package ru.practicum.comment.model;
+
+import lombok.*;
+import ru.practicum.event.model.Event;
+import ru.practicum.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments", schema = "public")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "timestamp")
+    private LocalDateTime createdOn;
+}
