a1ccaf0222f4ec59caa8d9f75ab67a057bfd5c1f
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2023-12-04 22:25:50 +0300

    Create CommentMapper.java

---
+++ ewm-main-service/src/main/java/ru/practicum/comment/mapper/CommentMapper.java
@@ -0,0 +1,29 @@
+package ru.practicum.comment.mapper;
+
+import ru.practicum.comment.dto.CommentDto;
+import ru.practicum.comment.model.Comment;
+import ru.practicum.util.exeption.CustomException;
+
+public final class CommentMapper {
+
+    private CommentMapper() {
+        throw new CustomException("Это служебный класс, и его экземпляр не может быть создан");
+    }
+
+    public static CommentDto toDto(Comment model) {
+        return CommentDto.builder()
+                .userName(model.getUser().getName())
+                .createdOn(model.getCreatedOn())
+                .text(model.getText())
+                .id(model.getId())
+                .build();
+    }
+
+    public static Comment fromDto(CommentDto dto) {
+        return Comment.builder()
+                .id(dto.getId())
+                .text(dto.getText())
+                .createdOn(dto.getCreatedOn())
+                .build();
+    }
+}
